57ea90429504ee04fbe3b3f7946e958b3508d346
Anton Bocan <bocan.anton@mail.ru>
2026-03-01 09:53:46 +0500

    Fix file upload: use native HTML input instead of file_picker

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "f0c38d4e185fc7fa3db291df5c185504",
+"flutter_bootstrap.js": "f38f8d6e678817ef6c3b7fa82bc1549b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "5b8e58fb0bfd19e624d58080d62070a9",
 "/": "5b8e58fb0bfd19e624d58080d62070a9",
-"main.dart.js": "a4590287fd873bdd3aa09f3be4f330dc",
+"main.dart.js": "2672318fdb9d1e7fe741e41b5f9e948e",
 "manifest.json": "28244ca7bb29856d0f9f9cc605b3c8af",
 "version.json": "68fa71887a6f44e73e7984c07fdaf197"};
 // The application shell files that are downloaded before a service worker can
